9ca5b163b4ee6f5e47e3c32fab58998aa384dc99
namu56 <swi920224@gmail.com>
2024-01-10 14:12:45 +0900

    style: parameter 변경

--- routes/books.js
+++ routes/books.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getAllBooks, getBookDetail } = require('../controllers/BookController');
 
 router.get('/', getAllBooks); // (카테고리별) 전체 도서 조회
-router.get('/:book_id', getBookDetail); // 개별 도서 조회
+router.get('/:bookId', getBookDetail); // 개별 도서 조회
 
 module.exports = router;
--- routes/orders.js
+++ routes/orders.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.post('/', order); // 주문하기
 router.get('/', getOrders); // 주문 목록 조회
-router.get('/:id', getOrderDetail); // 주문 상세 조회
+router.get('/:orderId', getOrderDetail); // 주문 상세 조회
 
 module.exports = router;
