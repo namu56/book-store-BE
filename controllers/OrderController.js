e4de22209d129a485803ec234ad1eadbbc46efaa
namu56 <swi920224@gmail.com>
2024-01-11 22:21:21 +0900

    feat: 주문 상세 조회 기능 추가

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -12,15 +12,15 @@ const order = async (req, res) => {
         // delivery 테이블 삽입
         let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)`;
         let values = [delivery.address, delivery.receiver, delivery.contact];
-        const [deliveryResults] = await conn.query(sql, values);
-        let deliveryId = deliveryResults.insertId;
+        const [deliveryResult] = await conn.query(sql, values);
+        let deliveryId = deliveryResult.insertId;
 
         // orders 테이블 삽입
         sql = `INSERT INTO orders (user_id, delivery_id, book_title, total_quantity, total_price)
            VALUES (?, ?, ?, ?, ?)`;
         values = [userId, deliveryId, firstBookTitle, totalQuantity, totalPrice];
-        const [orderResults] = await conn.query(sql, values);
-        let orderId = orderResults.insertId;
+        const [orderResult] = await conn.query(sql, values);
+        let orderId = orderResult.insertId;
 
         // 결제할 도서정보 장바구니에서 가져오기
         sql = `SELECT * FROM cartItems WHERE id IN (?)`;
@@ -32,13 +32,13 @@ const order = async (req, res) => {
         getCartItems.forEach((cartItem) => {
             values.push([orderId, cartItem.book_id, cartItem.quantity]);
         });
-        const [orderedBookResults] = await conn.query(sql, [values]);
+        const [orderedBookResult] = await conn.query(sql, [values]);
 
         // 결제된 도서 장바구니 삭제
         sql = `DELETE FROM cartItems WHERE id IN (?)`;
-        const [removeCartItemsResults] = await conn.query(sql, [cartItems]);
+        const [removeCartItemsResult] = await conn.query(sql, [cartItems]);
 
-        return res.status(StatusCodes.OK).json(removeCartItemsResults);
+        return res.status(StatusCodes.OK).json(removeCartItemsResult);
     } catch (err) {
         return handleQueryError(err, res);
     }
@@ -46,23 +46,40 @@ const order = async (req, res) => {
 
 // 주문 목록 조회
 const getOrders = async (req, res) => {
-    let sql = `SELECT orders.id,
-                      created_at,
-                      address,
-                      receiver,
-                      contact,
-                      book_title,
-                      total_quantity,
-                      total_price
+    let sql = `SELECT 
+                    orders.id,
+                    created_at,
+                    address,
+                    receiver,
+                    contact,
+                    book_title,
+                    total_quantity,
+                    total_price
                FROM orders
                LEFT JOIN delivery 
                ON orders.delivery_id = delivery.id;`;
-    let [getOrdersResults] = await conn.query(sql);
+    let [getOrdersResult] = await conn.query(sql);
 
-    return res.status(StatusCodes.OK).json(rows);
+    return res.status(StatusCodes.OK).json(getOrdersResult);
 };
 
 // 주문 상세 조회
-const getOrderDetail = (req, res) => {};
+const getOrderDetail = async (req, res) => {
+    const { orderId } = req.params;
+
+    let sql = `SELECT 
+                    book_id,
+                    title,
+                    author,
+                    price,
+                    quantity
+               FROM orderedBook
+               LEFT JOIN books 
+               ON orderedBook.book_id = books.id
+               WHERE order_id = ?;`;
+    let [getOrderDetailResult] = await conn.query(sql, orderId);
+
+    return res.status(StatusCodes.OK).json(getOrderDetailResult);
+};
 
 module.exports = { order, getOrders, getOrderDetail };
